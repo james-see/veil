739d22b37a25a225e84ee6fcb2ff3cbc70e023ae
James Campbell <james@jamescampbell.us>
2018-06-03 18:52:46 -0400

    updates

--- examples/actions/textme.php
+++ examples/actions/textme.php
@@ -1,6 +1,6 @@
 <?php
 $action = $_POST["action"];
 if ($action == 'textme'){
-	echo "<p class='shake-opacity shake-constant shake-constant--hover'>James Campbell is a security-minded technology-enabled artist who really doesnt want you to scrape his website. Welcome sentient beings. All others are banned. ;) ;) ;)</p><p>Contact: GPG public key digest: <a href='https://pgp.mit.edu/pks/lookup?search=0xF0DA9511&op=index'>F0DA9511</a>";
+	echo "<p class='shake-opacity shake-constant shake-constant--hover'>James Campbell is a security-minded technology-enabled artist who really doesnt want you to scrape his website. Welcome sentient beings. All others are banned. ;) ;) ;)</p><p>Contact: GPG public key digest: <a style='color:white;font-weight: 800;'href='https://pgp.mit.edu/pks/lookup?search=0xF0DA9511&op=index'>F0DA9511</a>";
 }
 ?>
